aef491039f358d4905f6789fcda21a320e9f71cf
Cynthia Hadinoto <cynthia.hadinoto@mobius.eu>
2025-10-19 23:34:29 +0200

    refactor: comment out dotenv import and related code

--- functions.py
+++ functions.py
@@ -4,10 +4,11 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import os
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 from supabase import create_client, Client
 
-load_dotenv()
+# load_dotenv()
 # url = os.getenv("SUPABASE_URL")
 # key = os.getenv("SUPABASE_ANON_KEY")
 # sb = create_client(url, key)
